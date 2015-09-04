441fe5a3ea34246e3ec3a0d72686aaa9444e5bea
tracer-ics2015 <tracer@njuics.org>
2015-09-04 11:09:24 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  11:09:24 up 10:37,  2 users,  load average: 0.13, 0.06, 0.05 5c012a30500d9bc8115190082a536a7199316e15

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,17 +14,20 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef union {
+typedef struct {
 	union {
-		uint32_t _32;
-		uint16_t _16;
-		uint8_t _8[2];
-	} gpr[8];
+		union {
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+		struct {
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
+	};
 	swaddr_t eip;
 
 } CPU_state;
