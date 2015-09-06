acf4f309a91669990feb606ffe54e8046a072686
tracer-ics2015 <tracer@njuics.org>
2015-09-06 09:48:08 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  09:48:08 up 23 min,  2 users,  load average: 0.13, 0.11, 0.12 bf16be3ec89d34cc61abb9ba4a267b6c14eddc85

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,15 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args) {
+	int x=0,i;
+	for(i=0;i<strlen(args);++i) {
+		x=x*10+args[i]-48;
+	}
+	cpu_exec(x);
+	return 0;
+}
+
 static struct {
 	char *name;
 	char *description;
@@ -46,7 +55,8 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+	{ "si", "Run the program by N command,default by one", cmd_si},
+	
 	/* TODO: Add more commands */
 
 };
