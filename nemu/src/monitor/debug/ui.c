ed1d210b2beb19a3b5c37de0b36b2d829efd7e78
tracer-ics2015 <tracer@njuics.org>
2015-09-07 11:46:46 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  11:46:46 up  6:36,  2 users,  load average: 0.18, 0.07, 0.06 994dba71805c9ccb230902fda1da89142af64205

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -33,7 +33,7 @@ char* rl_gets() {
 		line_read[i] = '\0';
 	}else {
 		for(i = 0; i < strlen(line_read); ++ i) {
-			if (i > 11) break;
+			if (i > 25) break;
 			last_command[i] = line_read[i];
 		}
 		last_command[i] = '\0';
