62b65cadf6ed884d174909bd65dee61c7bd14579
tracer-ics2015 <tracer@njuics.org>
2015-09-09 13:32:31 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  13:32:31 up  7:00,  2 users,  load average: 0.27, 0.17, 0.10 60b27ebd7d0065a956de5044f0f0745821291197

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -150,7 +150,7 @@ uint32_t select_op(int p, int q) {
 		if (in_par) continue;
 		if (tokens[i].level == min_level) {
 			if (tokens[i].level == 9) {
-				while(i > 0 && tokens[i-1].level == 9) {
+				while(i > p && tokens[i-1].level == 9) {
 					printf("%d",i);
 					i --;
 				}
