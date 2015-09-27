c4ee61bad05692341bc6b5ece12811aa1ef238c2
tracer-ics2015 <tracer@njuics.org>
2015-09-28 05:46:45 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:46:45 up  3:14,  1 user,  load average: 0.04, 0.08, 0.06 170f137b5290319498a404afc7393e4232e030e

--- nemu/src/cpu/exec/data-sub/sub.h
+++ nemu/src/cpu/exec/data-sub/sub.h
@@ -7,7 +7,7 @@ make_helper(sub_i2rm_b);
 //make_helper(mov_moffs2a_b);
 
 //make_helper(mov_i2r_v);
-//make_helper(mov_i2rm_v);
+make_helper(sub_i2rm_v);
 //make_helper(mov_r2rm_v);
 //make_helper(mov_rm2r_v);
 //make_helper(mov_a2moffs_v);
