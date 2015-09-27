77cf0cd90793dbbb54a5028d848d14dd90e40878
tracer-ics2015 <tracer@njuics.org>
2015-09-28 05:33:20 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:33:20 up  3:00,  1 user,  load average: 0.09, 0.06, 0.05 65f730f67db5feb864a1d8dc57e9ae43f1bb98a

--- nemu/src/cpu/exec/data-mov/sub.c
+++ nemu/src/cpu/exec/data-mov/sub.c
@@ -14,6 +14,11 @@
 
 /* for instruction encoding overloading */
 
+//make_helper(sub_i2rm_v);
+//make_helper(sub_i2rm_w);
+//make_helper(sub_i2rm_l);
+//make_helper(sub_i2rm_b);
+
 //make_helper_v(mov_i2r)
 make_helper_v(sub_i2rm)
 //make_helper_v(mov_r2rm)
