d372e471c5fec5a75cf9edd6f3fcd0fa4977ade6
tracer-ics2015 <tracer@njuics.org>
2015-09-28 11:33:56 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  11:33:56 up  9:01,  1 user,  load average: 0.21, 0.08, 0.06 8e5aed6ade16587f78ed8217fc79ee91f1c83049

--- nemu/src/cpu/exec/data-sub/sub-template.h
+++ nemu/src/cpu/exec/data-sub/sub-template.h
@@ -8,31 +8,10 @@ static void do_execute() {
 	op_dest->str[2]='s';op_dest->str[3]='p';
 	op_dest->str[4]='\0';
 	op_dest->val = REG(R_ESP);
-//	OPERAND_W(op_dest, op_dest->val - op_src->val);
 	cpu.esp = cpu.esp - op_src->val;
 	print_asm_template2();
 }
 
-//make_instr_helper(i2r)
 make_instr_helper(i2rm)
-//make_instr_helper(r2rm)
-//make_instr_helper(rm2r)
 
-/*
-make_helper(concat(mov_a2moffs_, SUFFIX)) {
-	swaddr_t addr = instr_fetch(eip + 1, 4);
-	MEM_W(addr, REG(R_EAX));
-
-	print_asm("mov" str(SUFFIX) " %%%s,0x%x", REG_NAME(R_EAX), addr);
-	return 5;
-}
-
-make_helper(concat(mov_moffs2a_, SUFFIX)) {
-	swaddr_t addr = instr_fetch(eip + 1, 4);
-	REG(R_EAX) = MEM_R(addr);
-
-	print_asm("mov" str(SUFFIX) " 0x%x,%%%s", addr, REG_NAME(R_EAX));
-	return 5;
-}
-*/
 #include "cpu/exec/template-end.h"
