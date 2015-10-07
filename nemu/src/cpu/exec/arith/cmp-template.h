3d9fe2960395b54be1f6def2dc05722580d4186b
tracer-ics2015 <tracer@njuics.org>
2015-10-08 06:19:16 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:19:16 up  1:42,  1 user,  load average: 0.00, 0.01, 0.05 42d1ba9efad48505c2fc41987a37eb8c932b206c

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -3,8 +3,11 @@
 #define instr cmp
 
 static void do_execute() {
-//	OPERAND_W(op_dest, op_dest->val - op_src->val);
-	
+	DATA_TYPE result = op_dest->val - op_src->val;
+	if(op_dest->val < op_src->val) cpu.CF = 1; else cpu.CF = 0;
+	if(MSB(op_dest->val) != MSB(op_src->val) && MSB(result) != MSB(op_dest->val)) 
+		cpu.OF = 1; else cpu.OF = 0;
+	Updata_EFLAGS(result);
 	print_asm_template2();
 }
 
