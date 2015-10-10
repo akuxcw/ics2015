d882092d56533da75089d57d57d92659aa2b4e31
tracer-ics2015 <tracer@njuics.org>
2015-10-11 06:02:38 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:02:38 up  1:42,  1 user,  load average: 0.17, 0.08, 0.06 2298596f5e55f5c316f68e50418a46ab4ec375f

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -17,7 +17,7 @@ make_instr_helper(i)
 
 static void do_execute() {
 	if(cpu.ZF == 1 || cpu.CF == 1) {
-		cpu.eip += op_src->val < 1 << (DATA_BYTE - 1) ? op_src->val : - ((1 << DATA_BYTE) - op_src->val);
+		cpu.eip += op_src->val < 1 << (4 * DATA_BYTE - 1) ? op_src->val : - ((1 << 4 * DATA_BYTE) - op_src->val);
 	}
 	print_asm_template1();
 }
