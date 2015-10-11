b11ef045d21fbcddbfad19b2730abd49619c1b70
tracer-ics2015 <tracer@njuics.org>
2015-10-11 18:43:28 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  18:43:28 up  1:26,  1 user,  load average: 0.06, 0.05, 0.05 e10c68e41e47840149f3f401b4888fbecad13dce

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -12,6 +12,14 @@ static void do_execute() {
 	print_asm_template2();
 }
 
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+
 make_instr_helper(si2rm)
+
+#endif
+
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
