59cf39384a2b9768774014f4615cd05dc67f9598
tracer-ics2015 <tracer@njuics.org>
2015-10-12 05:21:05 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:21:05 up 12:04,  1 user,  load average: 0.00, 0.04, 0.07 8e1b01a45493e657cc36eed59a5f94483afb0700

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -5,9 +5,9 @@
 static void do_execute() {
 	printf("%d\n", op_src->size);
 	printf("%x\n", op_src->val);
-	if(op_src->val >> (op_src->size - 1) == 1)
-		op_src->val |= ((1 << op_src->size)-1) << op_src->size;else 
-			op_src->val &= ((1 << op_src->size)-1);
+	if(op_src->val >> (8 * op_src->size - 1) == 1)
+		op_src->val |= ((1 << 8 * op_src->size)-1) << 8 * op_src->size;else 
+			op_src->val &= ((1 << 8 * op_src->size)-1);
 	printf("%x\n", op_src->val);
 	OPERAND_W(op_dest, op_src->val);
 	print_asm_template2();
