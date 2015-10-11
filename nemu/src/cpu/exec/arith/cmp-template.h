625d035d739922c7491a41bfab5228e1497e8e31
tracer-ics2015 <tracer@njuics.org>
2015-10-12 01:04:46 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  01:04:46 up  7:48,  1 user,  load average: 0.16, 0.06, 0.05 343da29861b686f06d709b0f838a78ec5067a8da

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -4,7 +4,6 @@
 
 static void do_execute() {
 	DATA_TYPE result = op_dest->val - op_src->val;
-//	OPERAND_W(op_dest, result);
 	if(op_dest->val < op_src->val) cpu.CF = 1; else cpu.CF = 0;
 	if(MSB(op_dest->val) != MSB(op_src->val) && MSB(result) != MSB(op_dest->val))
 		cpu.OF = 1; else cpu.OF = 0;
--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -3,6 +3,7 @@
 #define instr jmp
 
 static void do_execute() {
+	if(op_src->val>0x100000)swaddr_read(op_src->val, 4);
 	cpu.eip += op_src->val;
 //	printf("%x\n",cpu.eip);
 	print_asm_template1();
