4bd0cf954a5f6500f21fcbe312feb8fea250c622
tracer-ics2015 <tracer@njuics.org>
2015-10-12 01:41:02 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  01:41:02 up  8:24,  1 user,  load average: 0.19, 0.10, 0.06 6b4348fdf5b06d32a8abddac2d786a8e1079aedc

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -20,12 +20,12 @@ static make_helper(_2byte_esc);
 /* 0x80 */
 make_group(group1_b,
 	add_i2rm_b, or_i2rm_b, adc_i2rm_b, inv, 
-	and_i2rm_b, inv, xor_i2rm_b, cmp_i2rm_b)
+	and_i2rm_b, sub_i2rm_b, xor_i2rm_b, cmp_i2rm_b)
 
 /* 0x81 */
 make_group(group1_v,
 	add_i2rm_v, or_i2rm_v, adc_i2rm_v, inv, 
-	and_i2rm_v, inv, xor_i2rm_v, cmp_i2rm_v)
+	and_i2rm_v, sub_i2rm_v, xor_i2rm_v, cmp_i2rm_v)
 
 /* 0x83 */
 make_group(group1_sx_v,
