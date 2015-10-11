0b4dc84d408523ec1a0f00821c561139a8c68c9c
tracer-ics2015 <tracer@njuics.org>
2015-10-11 17:56:17 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  17:56:17 up 39 min,  1 user,  load average: 0.12, 0.06, 0.05 a5e2d54cf4cbcbaa2f00fb04ea5e3e16a2b1c01c

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -1,15 +1,15 @@
 #include "cpu/exec/template-start.h"
 
-#define instr push
+#define instr pop
 
 static void do_execute() {
-	cpu.esp -= DATA_BYTE;
-	swaddr_write(cpu.esp, DATA_BYTE, op_src->val);
+	DATA_TYPE result = swaddr_read(cpu.esp, DATA_BYTE);
+	cpu.esp += DATA_BYTE;
+	OPERAND_W(op_src, result); 
 	print_asm_template1();
 }
 
 make_instr_helper(rm)
 make_instr_helper(r)
-make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
