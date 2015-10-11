1c85e0447086d5efa5c57454492b3d5017779538
tracer-ics2015 <tracer@njuics.org>
2015-10-11 23:57:41 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:57:41 up  6:41,  1 user,  load average: 0.14, 0.07, 0.07 ff406dc8640b629ba509559bfcb848b0c2d4e4b8

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -3,8 +3,10 @@
 #define instr test
 
 static void do_execute() {
+	DATA_TYPE result = op_src->val & op_dest->val;
 	cpu.OF = cpu.CF = 0;
-	cpu.ZF = (op_src->val == op_dest->val);
+//	cpu.ZF = (op_src->val == op_dest->val);
+	Updata_EFLAGS(result);
 	print_asm_template2();
 }
 
