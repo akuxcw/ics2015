1f753cbda8e80c806a7bf617c226d3fd4153d0ee
tracer-ics2015 <tracer@njuics.org>
2015-10-13 06:17:07 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:17:07 up  3:12,  1 user,  load average: 0.14, 0.13, 0.08 48ccb5ed24e10999f66968fd488f3412c9cc110

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -5,6 +5,8 @@
 static void do_execute() {
 	op_src->type = op_dest->type = OP_TYPE_REG;
 	op_src->reg = R_ESI; op_dest->reg = R_EDI;
+	snprintf(op_src->str, OP_STR_SIZE, "%%%s", REG_NAME(R_ESI));
+	snprintf(op_dest->str, OP_STR_SIZE, "%%%s", REG_NAME(R_EDI));
 	swaddr_write(cpu.edi, DATA_BYTE, (DATA_TYPE) cpu.eax);
 	cpu.edi += cpu.DF == 0? +DATA_BYTE : -DATA_BYTE;
 	print_asm_template2();
