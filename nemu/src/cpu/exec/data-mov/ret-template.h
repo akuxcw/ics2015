962f6cfa5b03b3582152ab55b0ebb1ad13c9b345
tracer-ics2015 <tracer@njuics.org>
2015-10-13 04:51:59 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:51:59 up  1:47,  1 user,  load average: 0.11, 0.14, 0.12 1ab97430dea258eaad12e0db4824376e10b22d79

--- nemu/src/cpu/exec/data-mov/ret-template.h
+++ nemu/src/cpu/exec/data-mov/ret-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute() {
 //	cpu.esp += op_src->val;
-	DATA_TYPE result = swaddr_read(cpu.esp, 4);
+	uint32_t result = swaddr_read(cpu.esp, 4);
 	cpu.esp += 4 + op_src->val;
 	cpu.eip = result;
 	printf("%x\n",op_src->val);
