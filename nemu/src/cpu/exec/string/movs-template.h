ae2211c6d0de808bf7c934121f57e8b37b78913f
tracer-ics2015 <tracer@njuics.org>
2015-10-13 05:56:53 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:56:53 up  2:52,  1 user,  load average: 0.17, 0.07, 0.06 d1adbc52de95dc42178b801bada3290e3f961603

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -4,14 +4,9 @@
 
 static void do_execute() {
 //	OPERAND_W(op_dest, op_src->val);
-	swaddr_write(cpu.edi,4,swaddr_read(cpu.esi,4));
-	if(cpu.DF == 0) {
-		cpu.edi += 4;
-	   	cpu.esi += 4;
-	} else {
-		cpu.edi -= 4;
-		cpu.esi -= 4;
-	}
+	swaddr_write(cpu.edi, DATA_BYTE, swaddr_read(cpu.esi, DATA_BYTE));
+	cpu.edi += cpu.DF == 0? +DATA_BYTE : -DATA_BYTE;
+	cpu.esi += cpu.DF == 0? +DATA_BYTE : -DATA_BYTE;
 	print_asm_template2();
 }
 
