79220befd45c367a91d496e3d4b2fd8f8c601d28
tracer-ics2015 <tracer@njuics.org>
2015-10-13 06:28:10 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:28:10 up  3:23,  1 user,  load average: 0.01, 0.04, 0.05 e2cd4cb7ceefd46efa63371a1a263da58486c7c6

--- nemu/src/cpu/exec/string/cmps-template.h
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -5,8 +5,11 @@
 static void do_execute() {
 	op_src->type = op_dest->type = OP_TYPE_REG;
 	op_src->reg = R_ESI; op_dest->reg = R_EDI;
-	op_src->val = swaddr_read(cpu.esi, DATA_BYTE);
-	op_dest->val = swaddr_read(cpu.edi, DATA_BYTE);
+	snprintf(op_src->str, 11, "%%es:(%%edi)");
+	snprintf(op_dest->str, 11, "%%es:(%%esi)");
+	op_src->val = swaddr_read(cpu.edi, DATA_BYTE);
+	op_dest->val = swaddr_read(cpu.esi, DATA_BYTE);
+
 	DATA_TYPE result = op_dest->val - op_src->val;
 	if(op_dest->val < op_src->val) cpu.CF = 1; else cpu.CF = 0;
 	if(MSB(op_dest->val) != MSB(op_src->val) && MSB(result) != MSB(op_dest->val))
