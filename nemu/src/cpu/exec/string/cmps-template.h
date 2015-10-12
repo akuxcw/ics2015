48a0a511663fb5d592b0fa21c4a465f399179d14
tracer-ics2015 <tracer@njuics.org>
2015-10-13 04:31:02 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:31:02 up  1:26,  1 user,  load average: 0.01, 0.04, 0.05 b0a12e42b6ffc824feef481b7e02bf8142d4c1d9

---
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -0,0 +1,18 @@
+#include "cpu/exec/template-start.h"
+
+#define instr cmps
+
+static void do_execute() {
+	op_src->val = reg_b(R_ESI);
+	op_dest->val = reg_b(R_EDI);
+	DATA_TYPE result = op_dest->val - op_src->val;
+	if(op_dest->val < op_src->val) cpu.CF = 1; else cpu.CF = 0;
+	if(MSB(op_dest->val) != MSB(op_src->val) && MSB(result) != MSB(op_dest->val))
+		cpu.OF = 1; else cpu.OF = 0;
+	Updata_EFLAGS(result);
+	print_asm_template2();
+}
+
+make_instr_helper(n)
+
+#include "cpu/exec/template-end.h"
