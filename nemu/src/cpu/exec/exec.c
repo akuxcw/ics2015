efa8e1c7ba710d8e9a211e69f1a7dd0bea01f982
tracer-ics2015 <tracer@njuics.org>
2015-10-13 03:19:56 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  03:19:56 up 15 min,  1 user,  load average: 0.02, 0.07, 0.09 35e2196d36cbbb934010857a407ad662a2eac3f

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -64,12 +64,12 @@ make_group(group2_cl_v,
 
 /* 0xf6 */
 make_group(group3_b,
-	inv, inv, not_rm_b, neg_rm_b, 
+	test_i2rm_b, inv, not_rm_b, neg_rm_b, 
 	mul_rm_b, imul_rm2a_b, div_rm_b, idiv_rm_b)
 
 /* 0xf7 */
 make_group(group3_v,
-	inv, inv, not_rm_v, neg_rm_v, 
+	test_i2rm_v, inv, not_rm_v, neg_rm_v, 
 	mul_rm_v, imul_rm2a_v, div_rm_v, idiv_rm_v)
 
 /* 0xfe */
@@ -136,7 +136,7 @@ helper_fun opcode_table [256] = {
 /* 0x9c */	inv, inv, inv, inv,
 /* 0xa0 */	mov_moffs2a_b, mov_moffs2a_v, mov_a2moffs_b, mov_a2moffs_v,
 /* 0xa4 */	inv, inv, inv, inv,
-/* 0xa8 */	inv, inv, inv, inv,
+/* 0xa8 */	test_i2a_b, test_i2a_v, inv, inv,
 /* 0xac */	inv, inv, inv, inv,
 /* 0xb0 */	mov_i2r_b, mov_i2r_b, mov_i2r_b, mov_i2r_b,
 /* 0xb4 */	mov_i2r_b, mov_i2r_b, mov_i2r_b, mov_i2r_b,
--- nemu/src/cpu/exec/logic/test.h
+++ nemu/src/cpu/exec/logic/test.h
@@ -2,7 +2,11 @@
 #define __TEST_H__
 
 make_helper(test_r2rm_b);
+make_helper(test_i2rm_b);
+make_helper(test_i2a_b);
 
 make_helper(test_r2rm_v);
+make_helper(test_i2rm_v);
+make_helper(test_i2a_v);
 
 #endif
