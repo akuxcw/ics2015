ad77051d6a01c7e04ce9974155c53bc6e7e6dbc0
tracer-ics2015 <tracer@njuics.org>
2015-10-14 06:28:41 +0800

    >  run 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:28:41 up  7:20,  1 user,  load average: 0.09, 0.11, 0.07 520992f8b31e268e7b72d255871ae756d4870911

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -16,7 +16,7 @@ int main() {
 	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
 
 	FLOAT x1_ans = f2F(1.618);
-	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-3));
+	nemu_assert(Fabs(x1 - x1_ans) < f2F(1e-3));
 	FLOAT x2_ans = f2F(-1.618);
 
 	//nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
