526ab2057847c11d902f2ebb4291da5dfefc3e3d
tracer-ics2015 <tracer@njuics.org>
2015-10-14 19:37:36 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:37:36 up 10 min,  1 user,  load average: 0.08, 0.09, 0.08 b174b00d307c94443a3880a5b09af4abc22f1445

--- nemu/src/cpu/exec/data-mov/cltd-template.h
+++ nemu/src/cpu/exec/data-mov/cltd-template.h
@@ -3,7 +3,7 @@
 #define instr cltd
 
 static void do_execute() {
-	OPERAND_W(op_dest, op_src->val);
+//	OPERAND_W(op_dest, op_src->val);
 	if((cpu.eax >> 31) != 0) cpu.edx = 0xffffffff; else cpu.edx = 0x0;
 	print_asm("cltd");
 }
