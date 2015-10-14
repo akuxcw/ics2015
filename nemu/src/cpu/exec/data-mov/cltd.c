df4f71a69894090507e342158270fcba45d9a591
tracer-ics2015 <tracer@njuics.org>
2015-10-14 19:57:25 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:57:25 up 30 min,  1 user,  load average: 0.16, 0.17, 0.14 381e63b21243b829ae1e2f23c3aed6936580c9da

--- nemu/src/cpu/exec/data-mov/cltd.c
+++ nemu/src/cpu/exec/data-mov/cltd.c
@@ -1,5 +1,5 @@
 #include "cpu/exec/helper.h"
-/*
+
 #define DATA_BYTE 1
 #include "cltd-template.h"
 #undef DATA_BYTE
@@ -7,11 +7,12 @@
 #define DATA_BYTE 2
 #include "cltd-template.h"
 #undef DATA_BYTE
-*/
+
 #define DATA_BYTE 4
 #include "cltd-template.h"
 #undef DATA_BYTE
 
 /* for instruction encoding overloading */
 
+make_helper_v(cltd_n);
 
