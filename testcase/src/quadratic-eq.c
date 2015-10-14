225fb17f5488e04f09d986958114f9c4afcc0b4d
tracer-ics2015 <tracer@njuics.org>
2015-10-14 20:00:38 +0800

    >  run 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:00:38 up 33 min,  1 user,  load average: 0.01, 0.09, 0.12 bde919079449120346092a9da1ce819705e8a3

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -4,9 +4,9 @@
 /* solve 0.5x^2 + 0.5x - 0.5 = 0 */
 
 int main() {
-	FLOAT a = f2F(1.0);
-	FLOAT b = f2F(1.0);
-	FLOAT c = f2F(-1.0);
+	FLOAT a = f2F(0.5);
+	FLOAT b = f2F(0.5);
+	FLOAT c = f2F(-0.5);
 
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
