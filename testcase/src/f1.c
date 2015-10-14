67fb8c54f9e7d0f0d44491ba166ab962bfac73f5
tracer-ics2015 <tracer@njuics.org>
2015-10-14 21:57:27 +0800

    >  run 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  21:57:27 up  2:30,  1 user,  load average: 0.12, 0.09, 0.06 5644ab7f12e9361d4a911cdf9dd6be00e171706a

--- testcase/src/f1.c
+++ testcase/src/f1.c
@@ -3,9 +3,11 @@
 
 int main() {
 
-	FLOAT x = f2F(0.233);
-	FLOAT y = f2F(0.23301);
-	nemu_assert(Fabs(x - y) < f2F(1e-4));
+	FLOAT x = f2F(0.24);
+	FLOAT y = f2F(0.2);
+	FLOAT z = F_div_F(x, y);
+	FLOAT a = f2F(1.2);
+	nemu_assert(Fabs(z - a) < f2F(1e-4));
 
 	HIT_GOOD_TRAP;
 
