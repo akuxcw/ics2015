0a7c9ee14f5898c539a368e2d899062d41e309fe
tracer-ics2015 <tracer@njuics.org>
2015-10-14 23:25:29 +0800

    >  run 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:25:29 up  3:58,  1 user,  load average: 0.18, 0.10, 0.09 e86c03d5acaeff8c1361511aadcfef885ffcba5f

--- testcase/src/gcd.c
+++ testcase/src/gcd.c
@@ -5,9 +5,12 @@ int gcd(int a,int b){
 }
 
 int main() {
-	int a = 100;
-	int b = 10;
-	nemu_assert(gcd(a, b) == 10);
+	int a[] = {1,2,3,4,5,6,7,8,9,10};
+	int b[] = {101,102,103,104,105,106,107,108,109,110};
+	int c[] = {1,2,1,4,5,2,1,4,1,10};
+	int i;
+	for(i=0;i<10;++i)
+	nemu_assert(gcd(a[i], b[i]) == c[i]);
 
 	HIT_GOOD_TRAP;
 	return 0;
