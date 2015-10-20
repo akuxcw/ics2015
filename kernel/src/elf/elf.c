12f39f77bfd530f0321cf83053c0e74af581b32a
141220012-Chen Pengguang <141220012@smail.nju.edu.cn>
2015-10-21 06:43:09 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:43:09 up  1:20,  1 user,  load average: 0.08, 0.09, 0.06 861b3ca135c5e24a5036bb1f340c7f4863b94a8

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -32,25 +32,22 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-//	const uint32_t elf_magic = 0xc457f;
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
-//	printf("%x\n", *p_magic);
 	nemu_assert(*p_magic == elf_magic);
-
+	nemu_assert(*p_magic == elf_magic);
+	
 	/* Load each program segment */
-//	panic("please implement me");
-	for(; true; ) {
-//	for(; false; ) {
+//	for(; true; ) {
+	for(; false; ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
-			int i = 1;
-			if(i)break;
+//			int i;
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
+//			for(i = 0; i < ph->p_memsz; i ++) ramdisk_write(elf->e_entry + i, 1, buf[i]);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
