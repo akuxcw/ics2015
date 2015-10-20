d33521cd8b2be6b74372bbf2bc9fffde67c36a5a
tracer-ics2015 <tracer@njuics.org>
2015-10-21 00:37:09 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:37:09 up  1:32,  1 user,  load average: 0.25, 0.10, 0.07 6d2055f05ee74c1eff78a0c04674f210cb969be1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -294,6 +294,7 @@ uint32_t expr(char *e, bool *success) {
 		*success = false;
 		return 0;
 	}
+	print();
 	return eval(0,nr_token-1);
 	/* TODO: Insert codes to evaluate the expression. */
 	panic("please implement me");
