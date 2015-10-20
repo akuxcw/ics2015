38cb31102c786980005fa2128ed181099bb8f46a
141220012-Chen Pengguang <141220012@smail.nju.edu.cn>
2015-10-21 05:42:19 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:42:19 up 19 min,  1 user,  load average: 0.01, 0.03, 0.04 6e4688ed5fe614c22516a12b19c7d5a929c4bc4a

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,7 +31,8 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x464c457f;
+	const uint32_t elf_magic = 0xc457f;
+//	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 //	printf("%x\n", *p_magic);
 	nemu_assert(*p_magic == elf_magic);
