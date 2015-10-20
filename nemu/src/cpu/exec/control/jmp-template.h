45310b61fab3efd2fc8b14a76ef3776b918567a5
tracer-ics2015 <tracer@njuics.org>
2015-10-21 05:51:48 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:51:48 up 29 min,  1 user,  load average: 0.13, 0.08, 0.06 b4a1caf3b4adecc6ddf4c3bd04dcb141c1bc6

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -3,7 +3,7 @@
 #define instr jmp
 
 static void do_execute() {
-	printf("%x\n", op_src->val);
+	printf("%x %d\n", op_src->val, DATA_BYTE);
 	if(op_src->val>0x100000) {
 		op_src->val -= cpu.eip + 2;
 		printf("*****\n");
