44b0ebb6348a64fb406af5310248354b5cdc8d13
141220012-Chen Pengguang <141220012@smail.nju.edu.cn>
2015-10-21 08:07:07 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  08:07:07 up  2:44,  1 user,  load average: 0.08, 0.07, 0.05 2f7260d8ed3d4cfe3e4dd641c339e09f4d80714

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -39,7 +39,7 @@ uint32_t loader() {
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-	nemu_assert(elf->e_shstrndx == 0x5);
+	nemu_assert(elf->e_ehsize == 52);
 	nemu_assert(buf[50] == 0x5);
 	HIT_GOOD_TRAP;
 	/* Load each program segment */
