dbd4bc7d716ffe6f56dd76b3ef3fc3d7b0be2c53
tracer-ics2015 <tracer@njuics.org>
2015-10-23 04:16:46 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:16:46 up  2:36,  2 users,  load average: 0.08, 0.04, 0.05 bf103f0ee78e0bc8785eb6f031e028d96412e5d3

--- nemu/src/cpu/exec/data-mov/cmovcc-template.h
+++ nemu/src/cpu/exec/data-mov/cmovcc-template.h
@@ -111,6 +111,19 @@ make_instr_helper(rm)
 #undef instr
 */
 
+#define instr cmovs
+
+static void do_execute() {
+	if(cpu.SF == 1) {
+		 OPERAND_W(op_dest, op_src->val);
+	}
+	print_asm_template2();
+}
+
+make_instr_helper(rm2r)
+
+#undef instr
+
 #define instr cmovns
 
 static void do_execute() {
@@ -123,5 +136,6 @@ static void do_execute() {
 make_instr_helper(rm2r)
 
 #undef instr
+
 
 #include "cpu/exec/template-end.h"
