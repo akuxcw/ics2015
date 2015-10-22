a95d52ae15ed3d77dcaad2b8b0ebefe7628a062a
tracer-ics2015 <tracer@njuics.org>
2015-10-23 04:18:33 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:18:33 up  2:38,  2 users,  load average: 0.16, 0.07, 0.06 ac5716b746d9b48bbfdfcb4357d8f2163920cca4

--- nemu/src/cpu/exec/data-mov/cmovcc.c
+++ nemu/src/cpu/exec/data-mov/cmovcc.c
@@ -18,5 +18,7 @@ make_helper_v(cmove_rm2r)
 
 make_helper_v(cmovge_rm2r)
 
+make_helper_v(cmovs_rm2r)
+
 make_helper_v(cmovns_rm2r)
 
--- nemu/src/cpu/exec/data-mov/cmovcc.h
+++ nemu/src/cpu/exec/data-mov/cmovcc.h
@@ -16,6 +16,8 @@ make_helper(cmove_rm2r_v);
 
 make_helper(cmovge_rm2r_v);
 
+make_helper(cmovs_rm2r_v);
+
 make_helper(cmovns_rm2r_v);
 
 #endif
