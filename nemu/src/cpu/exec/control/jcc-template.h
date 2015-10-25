5b42eb466a9c5e0ca8122ef92e98723e54bfb48e
tracer-ics2015 <tracer@njuics.org>
2015-10-25 17:53:47 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  17:53:47 up 44 min,  1 user,  load average: 0.03, 0.06, 0.05 91abeafc8751d45a68127da071f1160d01de265

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,10 +1,16 @@
 #include "cpu/exec/template-start.h"
 
+extern int len;
+
+#define _exec() cpu.eip += op_src->val;\
+			    snprintf(op_src->str, OP_STR_SIZE, "$0x%x", cpu.eip + len + 1)
+
 #define instr ja
 
 static void do_execute() {
 	if(cpu.CF == 0 && cpu.ZF == 0) {
-		cpu.eip += op_src->val;
+		//cpu.eip += op_src->val;
+		_exec();
 	}
 	print_asm_template1();
 }
