5c00fc867a05441422d38e4902ea4242fb58314f
tracer-ics2015 <tracer@njuics.org>
2015-10-25 17:28:52 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  17:28:52 up 19 min,  1 user,  load average: 0.21, 0.09, 0.06 db6af55e3a256bebc7e2421d953d06e2d9d9ce3c

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -2,13 +2,16 @@
 
 #define instr call
 
+extern int len;
+
 static void do_execute() {
 	cpu.esp -= 4;
 	if(op_src->val < 0x100000 || 0xffffffff - op_src->val < 0x100000) {
-		swaddr_write(cpu.esp, 4, cpu.eip + 4);
+		swaddr_write(cpu.esp, 4, cpu.eip + len);
 		cpu.eip += op_src->val;
+		op_src->val = cpu.eip + len + 1;
 	}else {
-		swaddr_write(cpu.esp, 4, cpu.eip + 1);
+		swaddr_write(cpu.esp, 4, cpu.eip + len);
 		cpu.eip = op_src->val - 2;
 	}
 	print_asm_template1();
