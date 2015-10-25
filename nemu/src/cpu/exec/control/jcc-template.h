0edb0d47664cc884b362a4850de429e724a3dbe7
tracer-ics2015 <tracer@njuics.org>
2015-10-25 18:17:37 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  18:17:37 up  1:08,  1 user,  load average: 0.04, 0.07, 0.05 6b05594cac591ccc5e15a8a4dbef06bce5900e74

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -2,176 +2,165 @@
 
 extern int len;
 
-#define _exec() cpu.eip += op_src->val;\
-			    snprintf(op_src->str, OP_STR_SIZE, "$0x%x", cpu.eip + len + 1)
+#define if_exec() {cpu.eip += op_src->val;\
+				  snprintf(op_src->str, OP_STR_SIZE, "$0x%x", cpu.eip + len + 1);}
+
+#define else_exec() snprintf(op_src->str, OP_STR_SIZE, "$0x%x", cpu.eip + op_src->val + len + 1)
 
 #define instr ja
+#define JMPFLAG cpu.CF == 0 && cpu.ZF == 0
 
 static void do_execute() {
-	if(cpu.CF == 0 && cpu.ZF == 0) {
-		//cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #define instr jae
+#define JMPFLAG cpu.CF == 0
 
 static void do_execute() {
-	if(cpu.CF == 0) {
-		//cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #define instr jb
+#define JMPFLAG cpu.CF == 1
 
 static void do_execute() {
-	if(cpu.CF == 1) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #define instr je
+#define JMPFLAG cpu.ZF == 1
 
 static void do_execute() {
-	if(cpu.ZF == 1) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #define instr jbe
+#define JMPFLAG cpu.ZF == 1 || cpu.CF == 1
 
 static void do_execute() {
-	if(cpu.ZF == 1 || cpu.CF == 1) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #define instr jl
+#define JMPFLAG cpu.SF != cpu.OF
 
 static void do_execute() {
-	if(cpu.SF != cpu.OF) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
-
 #define instr jle
+#define JMPFLAG cpu.ZF == 1 || cpu.SF != cpu.OF
 
 static void do_execute() {
-	if(cpu.ZF == 1 || cpu.SF != cpu.OF) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #define instr jg
+#define JMPFLAG cpu.ZF == 0 && cpu.SF == cpu.OF
 
 static void do_execute() {
-	if(cpu.ZF == 0 && cpu.SF == cpu.OF) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG	
 #undef instr
 
 #define instr jge
+#define JMPFLAG cpu.SF == cpu.OF
 
 static void do_execute() {
-	if(cpu.SF == cpu.OF) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
-
+	
+#undef JMPFLAG
 #undef instr
 
 #define instr jne
+#define JMPFLAG cpu.ZF == 0
 
 static void do_execute() {
-	if(cpu.ZF == 0) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #define instr jns
+#define JMPFLAG cpu.SF == 0
 
 static void do_execute() {
-	if(cpu.SF == 0) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #define instr js
+#define JMPFLAG cpu.SF == 1
 
 static void do_execute() {
-	if(cpu.SF == 1) {
-//		cpu.eip += op_src->val;
-		_exec();
-	}
+	if(JMPFLAG) if_exec() else else_exec();
 	print_asm_template1();
 }
 
 make_instr_helper(si)
 
+#undef JMPFLAG
 #undef instr
 
 #include "cpu/exec/template-end.h"
