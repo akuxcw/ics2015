99e35c4df6ea0e8dda6d76452f6157d89c4e399a
tracer-ics2015 <tracer@njuics.org>
2015-11-18 01:44:08 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  01:44:08 up  6:14,  1 user,  load average: 0.48, 0.16, 0.09 c80b579dc2c2d4cb22f72e322ceaee46b02ffba8

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -14,7 +14,6 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	cache_write(addr, len, data);
 	L2_cache_write(addr, len, data);
-
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
