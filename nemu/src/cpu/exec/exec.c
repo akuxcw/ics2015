a63bce0c6bcfeef27b5bacf5f24326ebef880bda
tracer-ics2015 <tracer@njuics.org>
2015-11-26 05:31:22 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:31:22 up  5:02,  1 user,  load average: 0.16, 0.07, 0.06 fbfbb4d4f6513c4fbea8a3c2d56483a0a77a8513

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -231,6 +231,7 @@ helper_fun _2byte_opcode_table [256] = {
 
 make_helper(exec) {
 	ops_decoded.opcode = instr_fetch(eip, 1);
+	printf("exec\n");
 	return opcode_table[ ops_decoded.opcode ](eip);
 }
 
