d007e96311300beca4d462e931c844758737948f
tracer-ics2015 <tracer@njuics.org>
2015-11-26 23:23:04 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:23:04 up  3:30,  1 user,  load average: 0.02, 0.04, 0.05 b47f131c493228d39c59e416a3597ff494b2ca68

--- nemu/src/memory/sreg.c
+++ nemu/src/memory/sreg.c
@@ -11,8 +11,9 @@ lnaddr_t seg_translate(swaddr_t addr, uint8_t sreg) {
 	tmp[1] = lnaddr_read(cpu.GDTR.base + cpu.sr[sreg].index + 4, 4);
 	SegDesc *segdesc = (SegDesc*)tmp;
 	printf("%s\n", sregs[sreg]);
-		printf("%x\n", cpu.GDTR.base );
-	printf("%x %x %x\n", segdesc->base_31_24, segdesc->base_23_16, segdesc->base_15_0);
+//		printf("%x\n", cpu.GDTR.base );
+	printf("%x\n", 
+			(segdesc->base_31_24 << 24) + (segdesc->base_23_16 << 16) + segdesc->base_15_0 );
 	return 
 		(segdesc->base_31_24 << 24) + (segdesc->base_23_16 << 16) + 
 		segdesc->base_15_0 + addr;
