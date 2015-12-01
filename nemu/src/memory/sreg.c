2212111b3c4eadc9d34ccc4d6b9b3ab018e6bd65
tracer-ics2015 <tracer@njuics.org>
2015-12-02 06:01:56 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:01:56 up  6:21,  1 user,  load average: 0.12, 0.10, 0.11 56993af1aed7f0eb493c5185032caa539883b03

--- nemu/src/memory/sreg.c
+++ nemu/src/memory/sreg.c
@@ -14,7 +14,7 @@ lnaddr_t seg_translate(swaddr_t addr, uint8_t sreg) {
 //		printf("%x\n", cpu.GDTR.base );
 //	printf("%x\n", 
 //		(segdesc->base_31_24 << 24) + (segdesc->base_23_16 << 16) + segdesc->base_15_0 );
-	Assert(segdesc->present == 1, "Segdesc is not valid!");
+	if(sreg != R_CS) Assert(segdesc->present == 1, "Segdesc is not valid!");
 	return 
 		(segdesc->base_31_24 << 24) + (segdesc->base_23_16 << 16) + 
 		segdesc->base_15_0 + addr;
