1e06abcf8ac249b505b6f5392e693dbd3ff06206
tracer-ics2015 <tracer@njuics.org>
2015-12-02 02:17:00 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  02:17:00 up  2:36,  1 user,  load average: 0.00, 0.01, 0.05 fd36b070109467ea365c9fa69e28b5769e530537

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -24,12 +24,12 @@ hwaddr_t page_translate(lnaddr_t addr) {
 		hwaddr_read((cpu.cr._3.page_directory_base << 12) + 4 * lnaddr.dir, 4);
 	Assert(dir_entry.present == 1, "dir_entry is not valid!");
 
-	printf("0x%x\n", dir_entry.page_frame);
+	printf("0x%x\n", (dir_entry.page_frame << 12) + 4 * lnaddr.page);
 	PTE pg_tbl_entry;
 	pg_tbl_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
 	Assert(pg_tbl_entry.present == 1, "pg_tbl_entry is not valid!");
 
-	printf("0x%x\n", pg_tbl_entry.page_frame);
+	printf("0x%x\n", (pg_tbl_entry.page_frame << 12) + 4 * lnaddr.offset);
 	hwaddr_t hwaddr = hwaddr_read((pg_tbl_entry.page_frame << 12) + 4 * lnaddr.offset, 4);
 	printf("0x%x 0x%x\n", addr, hwaddr);
 	return hwaddr;
