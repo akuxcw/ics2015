c961320af84118f01aea40173db7d11427736311
tracer-ics2015 <tracer@njuics.org>
2015-12-02 00:32:25 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:32:25 up 51 min,  1 user,  load average: 0.07, 0.18, 0.15 450c68fd8013bbd374aece19c2f1d1ddbcc9c692

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -171,7 +171,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x14 */	inv, inv, inv, inv, 
 /* 0x18 */	inv, inv, inv, inv, 
 /* 0x1c */	inv, inv, inv, inv, 
-/* 0x20 */	movsp_rm2cr_l, inv, movsp_cr2rm_l, inv, 
+/* 0x20 */	movsp_cr2rm_l, inv, movsp_rm2cr_l, inv, 
 /* 0x24 */	inv, inv, inv, inv,
 /* 0x28 */	inv, inv, inv, inv, 
 /* 0x2c */	inv, inv, inv, inv, 
