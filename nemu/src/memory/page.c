c2d6231b2fe1fbbeee571a0e0722aeb673824156
tracer-ics2015 <tracer@njuics.org>
2015-12-02 01:31:51 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  01:31:51 up  1:51,  1 user,  load average: 0.00, 0.01, 0.05 ece5fe409b239d428295690771cba29d024fd89

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -2,9 +2,26 @@
 #include "../../lib-common/x86-inc/mmu.h"
 #include "cpu/reg.h"
 
+typedef union {
+	struct {
+		uint32_t offset	:	12;
+		uint32_t page	:	10;
+		uint32_t dir	:	10;
+	};
+	uint32_t val;
+} lnaddr_st;
+
 uint32_t hwaddr_read(lnaddr_t, size_t);
 
 hwaddr_t page_translate(lnaddr_t addr) {
 	printf("***\n");
-	return addr - 0xc0000000; 
+	lnaddr_st lnaddr;
+	lnaddr.val = addr;
+	PDE dir_entry;
+	dir_entry.val = hwaddr_read(cpu.cr3.page_directory_base + 4 * lnaddr.dir, 4);
+	PTE pg_tbl_entry;
+	pg_tbl_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
+	printf("0x%x 0x%x 0x%x\n", 
+			cpu.cr3.page_directory_base, dir_entry.page_frame, pg_tbl_entry.page_frame);
+	return hwaddr_read((pg_tbl_entry.page_frame << 12) + lnaddr.offset, 4);
 }
