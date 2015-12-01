1e535b44f51e11de8241b5767d9a78db3b48e774
tracer-ics2015 <tracer@njuics.org>
2015-12-02 03:30:38 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  03:30:38 up  3:49,  1 user,  load average: 0.19, 0.10, 0.06 e8e7d074963cd84ccafaf1d36b6bf6ecfbc4eab3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -48,10 +48,10 @@ uint32_t loader() {
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
 			
-//			uint32_t hwaddr = 
+			uint32_t hwaddr = 
 				mm_malloc(ph->p_vaddr, ph->p_memsz);
 
-			memcpy((void *)ph->p_vaddr, (void *)(buf + ph->p_offset), ph->p_filesz);
+			memcpy((void *)/*ph->p_vaddr*/hwaddr, (void *)(buf + ph->p_offset), ph->p_filesz);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
