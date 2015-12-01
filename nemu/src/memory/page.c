e7ca6ef3aaee6833a736e512ba097a774e097c64
tracer-ics2015 <tracer@njuics.org>
2015-12-02 02:34:52 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  02:34:52 up  2:54,  1 user,  load average: 0.04, 0.07, 0.05 3577becd39246df7d5d584c2bd7ad8cb97465932

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -18,20 +18,22 @@ hwaddr_t page_translate(lnaddr_t addr) {
 	lnaddr_st lnaddr;
 	lnaddr.val = addr;
 	
-//	printf("0x%x\n", cpu.cr._3.page_directory_base);
+	bool flag = (addr == 0xc01012c5);
+
+	if(flag)printf("0x%x\n", cpu.cr._3.page_directory_base);
 	PDE dir_entry;
 	dir_entry.val = 
 		hwaddr_read((cpu.cr._3.page_directory_base << 12) + 4 * lnaddr.dir, 4);
 	Assert(dir_entry.present == 1, "dir_entry is not valid!");
 
-//	printf("0x%x\n", (dir_entry.page_frame << 12) + 4 * lnaddr.page);
+	if(flag)printf("0x%x\n", (dir_entry.page_frame << 12) + 4 * lnaddr.page);
 	PTE pg_tbl_entry;
 	pg_tbl_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
 	Assert(pg_tbl_entry.present == 1, "pg_tbl_entry is not valid!");
 
-//	printf("0x%x\n", (pg_tbl_entry.page_frame << 12) + 4 * lnaddr.offset);
+	if(flag)printf("0x%x\n", (pg_tbl_entry.page_frame << 12) + 4 * lnaddr.offset);
 	hwaddr_t hwaddr = (pg_tbl_entry.page_frame << 12) + lnaddr.offset;
 		//hwaddr_read((pg_tbl_entry.page_frame << 12) + 4 * lnaddr.offset, 4);
-//	printf("0x%x 0x%x\n", addr, hwaddr);
+	if(flag)printf("0x%x 0x%x\n", addr, hwaddr);
 	return hwaddr;
 }
