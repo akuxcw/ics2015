b6f18e641a56a901d1a20fcc4bcce461c2c7ec31
tracer-ics2015 <tracer@njuics.org>
2015-12-08 04:40:28 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:40:28 up 44 min,  1 user,  load average: 0.04, 0.09, 0.10 9685205e5409c33b2c8856b06fa4b123d6747d90

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,4 +1,4 @@
-#define MAX_LEVEL I
+#define MAX_LEVEL II
 
 #define COL_WIDTH 6
 #define LINE_WIDTH 3
@@ -8,15 +8,14 @@
 #define LEVEL I
 #include "cache-template.h"
 
-/*
 #define COL_WIDTH 6
 #define LINE_WIDTH 4
 #define SET_WIDTH 12
 #define FLAG_WIDTH (27 - COL_WIDTH - SET_WIDTH)
 #define WRITE_BACK
+#define WRITE_ALLOCATE
 #define LEVEL II
 #include "cache-template.h"
-*/
 
 void init_cache() {
 	init_cache_I();
