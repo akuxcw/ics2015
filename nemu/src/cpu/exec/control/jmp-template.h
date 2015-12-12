81590f4477be505ef09916761919027bdeb04e3a
tracer-ics2015 <tracer@njuics.org>
2015-12-13 02:35:10 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  02:35:10 up  6:28,  1 user,  load average: 0.15, 0.16, 0.10 c0606759670be86d6a3723a0c9b117755e918a0b

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -23,7 +23,7 @@ make_helper(concat(ljmp_, SUFFIX)) {
 	swaddr_t addr = instr_fetch(eip + 1, 4);
 	uint16_t sreg = instr_fetch(eip + 5, 2);
 	cpu.cs = sreg;
-	load_sreg(R_SS);
+	load_sreg(R_CS);
 //	printf("%x\n", cpu.cs);
 	cpu.eip = addr - 7;
 	print_asm("ljmp" str(SUFFIX) " $0x%x,$0x%x", sreg, addr);
