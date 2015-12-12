0ac0b097a841b80f901acda34e6c8430d854c3b1
tracer-ics2015 <tracer@njuics.org>
2015-12-13 00:11:21 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:11:21 up  4:04,  1 user,  load average: 0.14, 0.06, 0.05 e9e54625a1f5fdf5979c83a4d22015244b7412ce

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -7,6 +7,7 @@ static void do_execute() {
 	cpu.GDTR.base = lnaddr_read(op_src->addr + 2, 4);
 	printf("0x%x 0x%x %s\n", cpu.GDTR.limit, cpu.GDTR.base, op_src->str);
 	printf("%x %x %x %x\n", cpu.sr[0].index, cpu.sr[1].index, cpu.sr[2].index, cpu.sr[3].index);
+	printf("%x %x %x %x\n", cpu.sr[0]._16, cpu.sr[1]._16, cpu.sr[2]._16, cpu.sr[3]._16);
 	printf("%x %x %x %x\n", cpu.es, cpu.cs, cpu.ss, cpu.ds);
 	print_asm_template1();	
 }
