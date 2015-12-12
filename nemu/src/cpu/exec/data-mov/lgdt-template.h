1b85b8d7735e9fb1eeadfc9372a5c6e0adfed80c
tracer-ics2015 <tracer@njuics.org>
2015-12-12 23:25:14 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:25:14 up  3:18,  1 user,  load average: 0.12, 0.07, 0.08 497aeaffa17ad6614f5217a2e04facb218af8d9d

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -2,17 +2,26 @@
 
 #define instr lgdt
 
-uint32_t read_ModR_M(uint32_t, Operand*, Operand*);
+static void do_execute() {
+	cpu.GDTR.limit = lnaddr_read(op_src->val, 2);
+	cpu.GDTR.base = lnaddr_read(op_src->val + 2, 4);
+	print_asm_template1();
+	
+}
 
+make_instr_helper(rm);
+
+/*
+uint32_t read_ModR_M(uint32_t, Operand*, Operand*);
 make_helper(concat(lgdt_, SUFFIX)) {
 	Operand *rm = op_src;
 	Operand *reg = op_dest;
 	
 	int len = read_ModR_M(eip + 1, rm, reg);
-/*	lnaddr_t addr = instr_fetch(eip + 2, 4);
+	lnaddr_t addr = instr_fetch(eip + 2, 4);
 	cpu.GDTR.limit = lnaddr_read(addr, 2);
 	cpu.GDTR.base = lnaddr_read(addr + 2, 4);
-*/
+
 	panic("hahaha");	
 	cpu.GDTR.limit = lnaddr_read(op_src->val, 2);
 	cpu.GDTR.base = lnaddr_read(op_src->val + 2, 4);
@@ -21,4 +30,5 @@ make_helper(concat(lgdt_, SUFFIX)) {
 	print_asm_template1();
 	return len + 1;
 }
+*/
 #include "cpu/exec/template-end.h"
