4badff0971a2a12dc59270e990b6aa6fbddb8840
tracer-ics2015 <tracer@njuics.org>
2015-12-12 23:21:01 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:21:01 up  3:14,  1 user,  load average: 0.01, 0.06, 0.09 e5ff3f50c3c443cfe31201a0e05696f329db0c41

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -8,7 +8,7 @@ make_helper(concat(lgdt_, SUFFIX)) {
 	Operand *rm = op_src;
 	Operand *reg = op_dest;
 	
-	int len = read_ModR_M(eip, rm, reg);
+	int len = read_ModR_M(eip + 1, rm, reg);
 /*	lnaddr_t addr = instr_fetch(eip + 2, 4);
 	cpu.GDTR.limit = lnaddr_read(addr, 2);
 	cpu.GDTR.base = lnaddr_read(addr + 2, 4);
