7dbc56ead8a3d85b9a9e8c3a104256023ab0f3ea
tracer-ics2015 <tracer@njuics.org>
2015-12-13 04:50:46 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:50:46 up  8:44,  1 user,  load average: 0.00, 0.01, 0.05 684f2d89a7ad183ee2094a85d4a79a059be95d58

--- nemu/src/memory/sreg.c
+++ nemu/src/memory/sreg.c
@@ -12,8 +12,8 @@ void load_sreg(uint32_t sreg) {
 	for(i = 0; i < 8; ++ i) 
 		tmp[i] = lnaddr_read(cpu.GDTR.base + cpu.sr[sreg].index * 8 + i, 1);
 	SegDesc *segdesc = (SegDesc*)tmp;
-	Assert(segdesc->present == 1, "Segdesc is not valid! 0x%x", cpu.GDTR.base + cpu.sr[sreg].index * 8);
-	Assert(cpu.sr[sreg].index * 8 < (segdesc->limit_19_16 << 16) + segdesc->limit_15_0, "Segment overflow!");
+//	Assert(segdesc->present == 1, "Segdesc is not valid! 0x%x", cpu.GDTR.base + cpu.sr[sreg].index * 8);
+//	Assert(cpu.sr[sreg].index * 8 < (segdesc->limit_19_16 << 16) + segdesc->limit_15_0, "Segment overflow!");
 	cpu.sr[sreg].invi = *segdesc;
 }
 
