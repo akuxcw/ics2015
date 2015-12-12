184b9d8a784506ecf8505cf9f81b8558be79f5c4
tracer-ics2015 <tracer@njuics.org>
2015-12-13 05:01:17 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:01:17 up  8:54,  1 user,  load average: 0.05, 0.04, 0.05 28cbc6e86aaae9952a4c19f592358b97149bc0a

--- nemu/src/cpu/exec/intr.c
+++ nemu/src/cpu/exec/intr.c
@@ -5,7 +5,7 @@ extern jmp_buf jbuf;
 
 uint32_t lnaddr_read(lnaddr_t, size_t);
 void load_sreg(uint32_t);
-int tyu = 0;
+//int tyu = 0;
 
 void raise_intr(uint8_t NO) {
 	/* TODO: Trigger an interrupt/exception with ``NO''.
@@ -15,16 +15,16 @@ void raise_intr(uint8_t NO) {
 //	printf("%x\n", jbuf);
 	uint8_t tmp[8];
 	int i;
-	printf("0x%x\n", cpu.IDTR.base + NO * 0x8);
+//	printf("0x%x\n", cpu.IDTR.base + NO * 0x8);
 	for(i = 0; i < 8; ++ i) tmp[i] = lnaddr_read(cpu.IDTR.base + NO * 0x8 + i, 1);
-	for(i = 0; i < 8; ++ i) printf("%x\n", tmp[i]);
+//	for(i = 0; i < 8; ++ i) printf("%x\n", tmp[i]);
 	GateDesc *gde = (GateDesc*)tmp;
 	cpu.ss = gde->segment;
-	printf("%x\n", gde->segment);
+//	printf("%x\n", gde->segment);
 	cpu.eip = (gde->offset_31_16 << 16) + gde->offset_15_0;
-	printf("0x%x\n", cpu.eip);
+//	printf("0x%x\n", cpu.eip);
 	load_sreg(R_SS);
 	/* Jump back to cpu_exec() */
-	tyu = 1;
+//	tyu = 1;
 	longjmp(jbuf, 1);
 }
