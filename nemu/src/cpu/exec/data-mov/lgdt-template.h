ca2f655072476a64adc2ce53a3bfaed2f43ddcbe
tracer-ics2015 <tracer@njuics.org>
2015-12-12 23:18:32 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:18:32 up  3:11,  1 user,  load average: 0.08, 0.10, 0.10 6fac4d7901bb45690e57c61a71e969d3175b603

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -2,11 +2,22 @@
 
 #define instr lgdt
 
+uint32_t read_ModR_M(uint32_t, Operand*, Operand*);
+
 make_helper(concat(lgdt_, SUFFIX)) {
-	lnaddr_t addr = instr_fetch(eip + 2, 4);
+	Operand *rm = op_src;
+	Operand *reg = op_dest;
+	
+	int len = read_ModR_M(eip, rm, reg);
+/*	lnaddr_t addr = instr_fetch(eip + 2, 4);
 	cpu.GDTR.limit = lnaddr_read(addr, 2);
 	cpu.GDTR.base = lnaddr_read(addr + 2, 4);
-	print_asm("lgdt" str(SUFFIX) " 0x%x", addr); 
-	return 6;
+*/	
+	cpu.GDTR.limit = lnaddr_read(op_src->val, 2);
+	cpu.GDTR.base = lnaddr_read(op_src->val + 2, 4);
+	
+//	print_asm("lgdt" str(SUFFIX) " 0x%x", addr); 
+	print_asm_template1();
+	return len + 1;
 }
 #include "cpu/exec/template-end.h"
