9635ecfed80816f99af0c39039953ccc485edf86
tracer-ics2015 <tracer@njuics.org>
2015-12-13 04:18:52 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:18:52 up  8:12,  1 user,  load average: 0.22, 0.12, 0.07 6c20173bea4bb9b95ee4afff0258249b9be249

--- nemu/src/cpu/exec/intr.c
+++ nemu/src/cpu/exec/intr.c
@@ -4,6 +4,7 @@
 extern jmp_buf jbuf;
 
 uint32_t lnaddr_read(lnaddr_t, size_t);
+void load_sreg(uint32_t);
 
 void raise_intr(uint8_t NO) {
 	/* TODO: Trigger an interrupt/exception with ``NO''.
@@ -15,6 +16,7 @@ void raise_intr(uint8_t NO) {
 	for(i = 0; i < 8; ++ i) lnaddr_read(cpu.IDTR.base + NO * 0x8 + i, 1);
 	GateDesc *gde = (GateDesc*)tmp;
 	cpu.ss = gde->segment;
+	load_sreg(R_SS);
 	cpu.eip = (gde->offset_31_16 << 16) + gde->offset_15_0;
 	/* Jump back to cpu_exec() */
 	longjmp(jbuf, 1);
