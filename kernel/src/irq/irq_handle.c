f99197b6648e4e916850eb021bbc1a454f9be172
tracer-ics2015 <tracer@njuics.org>
2015-12-13 19:45:53 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:45:53 up  1:21,  1 user,  load average: 0.08, 0.08, 0.07 2adc1c279e48d9199a11e44ddcd9a3e1be2a144

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -41,7 +41,7 @@ void irq_handle(TrapFrame *tf) {
 	assert(tf->ecx == 0x80480ac);
 	assert(tf->edx == 0xe);
 	assert(tf->ebx == 0x1);
-//	assert(tf->old_esp == 0xbfffffd4);
+	assert(tf->old_esp == 0xbfffffd4);
 //	assert(tf->ebp == 0xbfffffe8);
 //	assert(tf->esi == 0x5aaf857e);
 //	assert(tf->edi == 0x6fe32f35);
