39d411d5a75f76ffbca9adf40a566bb2fe5e2339
tracer-ics2015 <tracer@njuics.org>
2015-12-14 00:36:02 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:36:02 up  6:11,  1 user,  load average: 0.19, 0.11, 0.07 3764ec4c8dcb992563057500376bbe8d250c7797

--- nemu/src/cpu/exec/data-mov/iret-template.h
+++ nemu/src/cpu/exec/data-mov/iret-template.h
@@ -6,14 +6,14 @@ void load_sreg(uint32_t);
 
 static void do_execute() {
 	cpu.eip = swaddr_read(cpu.esp, 4, R_SS);
-	cpu.esp += 4 + op_src->val;
+	cpu.esp += 4;// + op_src->val;
 	
 	cpu.cs = swaddr_read(cpu.esp, 4, R_SS);
-	cpu.esp += 4 + op_src->val;
+	cpu.esp += 4;// + op_src->val;
 	load_sreg(R_CS);
 
 	cpu.EFLAGS = swaddr_read(cpu.esp, 4, R_SS);
-	cpu.esp += 4 + op_src->val;
+	cpu.esp += 4;// + op_src->val;
 	
 	print_asm("iret");
 }
