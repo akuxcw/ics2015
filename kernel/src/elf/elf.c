f7761837bd89173089fc26fef4dca0068031f904
tracer-ics2015 <tracer@njuics.org>
2015-12-14 02:03:31 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  02:03:31 up  7:39,  1 user,  load average: 0.12, 0.08, 0.08 9c77659fdbbc5a229bad9ef3850cef52913a541

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -55,8 +55,7 @@ uint32_t loader() {
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 			
-//			for(i = ph->p_filesz; i < ph->p_memsz; i ++) 
-				memset((void *)hwaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			memset((void *)hwaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
