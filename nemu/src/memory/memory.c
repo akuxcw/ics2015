60dcc5b16a2ca7a6d55c1aa86f023abc7cf075a7
tracer-ics2015 <tracer@njuics.org>
2015-12-17 02:00:47 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  02:00:47 up 37 min,  1 user,  load average: 0.10, 0.10, 0.07 6ad4e162da4626aa62b7e963990790eac5dc19a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -2,17 +2,25 @@
 #include "cpu/reg.h"
 
 uint32_t cache_read(hwaddr_t, size_t);
-
 void cache_write(hwaddr_t, size_t, uint32_t);
+
+uint32_t mmio_read(uint32_t, size_t);
+void mmio_write(uint32_t, size_t, uint32_t);
+
+uint32_t is_mmio(hwaddr_t);
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	int32_t num = is_mmio(addr);
+	return num == -1 ? cache_read(addr, len) & (~0u >> ((4 - len) << 3)) :
+		mmio_read(num, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	cache_write(addr, len, data);
+	int32_t num = is_mmio(addr);
+	num == -1 ? cache_write(addr, len, data) :
+		mmio_write(num, len, data);
 }
 
 hwaddr_t page_translate(lnaddr_t, uint32_t);
