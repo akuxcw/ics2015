372665ef82ac0eacebf1079dfaefd7b4cc249eab
tracer-ics2015 <tracer@njuics.org>
2015-12-17 06:49:41 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:49:41 up  3:05,  1 user,  load average: 0.14, 0.10, 0.11 59245448d43da74080fbdc43c94c8bbcdf0b31dc

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -64,7 +64,7 @@ uint32_t loader() {
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 			
-			Log("%x", hwaddr + ph->p_filesz);
+			Log("%x", ph->p_vaddr + ph->p_filesz);
 			memset((void *)hwaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
