80529b78e0b5e0b54152f174b7dfa81506858583
tracer-ics2015 <tracer@njuics.org>
2015-12-17 07:04:00 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  07:04:00 up  3:19,  1 user,  load average: 0.19, 0.19, 0.16 9639f49abfdc4b172c64aea0b435c17a3a319a51

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -34,7 +34,7 @@ uint32_t loader() {
 #endif
 	elf = (void*)buf;
 
-//	HIT_GOOD_TRAP;
+	HIT_GOOD_TRAP;
 	/* TODO: fix the magic number with the correct one */
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
