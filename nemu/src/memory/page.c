3fd0bf138e464916c8ef1e5f2d425cd3d7f89b9b
tracer-ics2015 <tracer@njuics.org>
2015-12-17 05:34:24 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:34:24 up  1:50,  1 user,  load average: 0.10, 0.12, 0.12 d4fd8b433ebb147b7187e5e81460573a1990974a

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -24,12 +24,13 @@ PTE page_read(lnaddr_t addr, uint32_t len) {
 	PDE dir_entry;
 	dir_entry.val = 
 		hwaddr_read((cpu.cr._3.page_directory_base << 12) + 4 * lnaddr.dir, 4);
-	Assert(lnaddr.dir + len < limit, "Cross %x %x", lnaddr.dir, len);
+	Assert(lnaddr.dir + len < limit, "Cross page! %x %x", lnaddr.dir, len);
 	Assert(dir_entry.present == 1, "dir_entry is not valid!  0x%x", addr);
 
 	PTE pg_tbl_entry;
 	pg_tbl_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
-	Assert(pg_tbl_entry.present == 1, "pg_tbl_entry is not valid!  0x%x", addr);
+	Assert(pg_tbl_entry.present == 1, "pg_tbl_entry is not valid!  0x%x %x", 
+			addr, dir_entry.page_frame);
 
 //	hwaddr_t hwaddr = (pg_tbl_entry.page_frame << 12) + lnaddr.offset;
 	return pg_tbl_entry;
