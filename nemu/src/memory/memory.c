acc1d0d69a9ca248a2fe77d74592a888bdc8644d
tracer-ics2015 <tracer@njuics.org>
2015-12-17 04:35:53 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:35:53 up 51 min,  1 user,  load average: 0.12, 0.07, 0.05 627a5cf9e10eddc9defc8b15c897d19c19bd5d7a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -4,25 +4,25 @@
 uint32_t cache_read(hwaddr_t, size_t);
 void cache_write(hwaddr_t, size_t, uint32_t);
 
-uint32_t mmio_read(uint32_t, size_t);
-void mmio_write(uint32_t, size_t, uint32_t);
+uint32_t mmio_read(hwaddr_t, size_t, int);
+void mmio_write(hwaddr_t, size_t, uint32_t, int);
 
 uint32_t is_mmio(hwaddr_t);
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	int32_t num = is_mmio(addr);
-	if(num != -1) printf("^_^ %x\n", addr);
-	return num == -1 ? cache_read(addr, len) & (~0u >> ((4 - len) << 3)) :
-		mmio_read(num, len) & (~0u >> ((4 - len) << 3));
+	int map_NO = is_mmio(addr);
+//	if(num != -1) printf("^_^ %x\n", addr);
+	return map_NO == -1 ? cache_read(addr, len) & (~0u >> ((4 - len) << 3)) :
+		mmio_read(addr, len, map_NO) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	int32_t num = is_mmio(addr);
-	if(num != -1) printf("^_^ %x\n", addr);
-	num == -1 ? cache_write(addr, len, data) :
-		mmio_write(num, len, data);
+	int32_t map_NO = is_mmio(addr);
+	if(map_NO != -1) printf("^_^ %x\n", addr);
+	map_NO == -1 ? cache_write(addr, len, data) :
+		mmio_write(addr, len, data, map_NO);
 }
 
 hwaddr_t page_translate(lnaddr_t, uint32_t);
