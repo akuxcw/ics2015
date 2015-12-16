96681613e7d743428dcfc96066d69f41dab6ca86
tracer-ics2015 <tracer@njuics.org>
2015-12-17 04:03:14 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  04:03:14 up 19 min,  1 user,  load average: 0.03, 0.08, 0.05 3f1ac53e7f13ee9fff7341c1547eed728469c488

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -20,7 +20,8 @@ void create_video_mapping() {
 void video_mapping_write_test() {
 	int i;
 	uint32_t *buf = (void *)(VMEM_ADDR);
-//	HIT_GOOD_TRAP;
+	*buf = 0;
+	HIT_GOOD_TRAP;
 	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		buf[i] = i;
 		assert(0);
