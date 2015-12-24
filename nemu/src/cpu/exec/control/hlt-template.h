0434482610bc3c2efca050a2a104120fd4166444
tracer-ics2015 <tracer@njuics.org>
2015-12-25 03:38:09 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  03:38:09 up 28 min,  1 user,  load average: 0.19, 0.13, 0.13 68cb9342455e6111983ba6c16a67e15e7761a15d

--- nemu/src/cpu/exec/control/hlt-template.h
+++ nemu/src/cpu/exec/control/hlt-template.h
@@ -5,10 +5,10 @@
 //void raise_intr(uint8_t, uint32_t);
 
 static void do_execute() {
-//	cpu.eip --;
+	cpu.eip --;
 	Assert(cpu.IF == 1,"hlt!!");
-	while(!(cpu.INTR & cpu.IF));
-	print_asm("hlt");
+//	while(!(cpu.INTR & cpu.IF));
+//	print_asm("hlt");
 }
 
 make_instr_helper(n)
