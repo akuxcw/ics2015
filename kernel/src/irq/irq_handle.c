0427baa3d91c658a43bda4096d7dae88b8883581
tracer-ics2015 <tracer@njuics.org>
2015-12-25 06:07:08 +0800

    >  compile NEMU 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:07:08 up  2:57,  1 user,  load average: 0.32, 0.31, 0.22 acc85f69ad1a7b77cda7c22c3ffae537140d16e4

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -41,10 +41,9 @@ void irq_handle(TrapFrame *tf) {
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
 		do_syscall(tf);
-	} else if (irq < 1000 && irq != 255) {
-//		if(irq == 255) do_syscall(tf); else
+	} else if (irq < 1000) {
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
-	} else if (irq >= 1000 || irq == 255) {
+	} else if (irq >= 1000) {
 		int irq_id = irq - 1000;
 		assert(irq_id < NR_HARD_INTR);
 		struct IRQ_t *f = handles[irq_id];
