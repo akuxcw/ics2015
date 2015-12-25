543265613be184efd43807f682a34fb5c42e354a
tracer-ics2015 <tracer@njuics.org>
2015-12-26 06:51:45 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:51:45 up 1 min,  1 user,  load average: 0.42, 0.26, 0.10 364fe18de7fe171cd1e262b56bb670ad5cb44539

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -25,7 +25,6 @@ static void sys_write(TrapFrame *tf) {
 }
 
 void do_syscall(TrapFrame *tf) {
-	Log("%d\n", tf->eax);
 	switch(tf->eax) {
 		/* The ``add_irq_handle'' system call is artificial. We use it to 
 		 * let user program register its interrupt handlers. But this is 
