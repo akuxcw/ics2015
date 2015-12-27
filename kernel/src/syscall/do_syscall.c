4b2e681356d3df6e6f5374227490293ab53165a7
tracer-ics2015 <tracer@njuics.org>
2015-12-28 03:02:32 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  03:02:32 up  3:38,  1 user,  load average: 0.03, 0.05, 0.05 7361c55d4741fbd33278073cd622bc68e5b4e5b1

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -45,7 +45,9 @@ void do_syscall(TrapFrame *tf) {
 
 		case SYS_brk: /*panic("@@@");*/sys_brk(tf); break;
 		case SYS_write: sys_write(tf); break;
-		case SYS_open : tf->eax = fs_open((char *)tf->ecx, tf->edx); break;
+		case SYS_open : 
+			Log("%x %x %x\n", tf->ebx, tf->ecx, tf->edx);	
+			tf->eax = fs_open((char *)tf->ecx, tf->edx); break;
 		case SYS_read : tf->eax = fs_read(tf->ebx, (void *)tf->ecx, tf->edx); break;
 		case SYS_lseek : tf->eax = fs_lseek(tf->ebx, tf->ecx, tf->edx); break;
 		case SYS_close : tf->eax = fs_close(tf->ebx); break;
