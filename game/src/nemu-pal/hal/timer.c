d620c10b81dce3447ff68ad89c84e58fcf5a1181
tracer-ics2015 <tracer@njuics.org>
2015-12-28 05:33:06 +0800

    >  compile game 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  05:33:06 up  1:06,  1 user,  load average: 0.37, 0.29, 0.21 f35e7da7fe9aac9942fc749ca2104aed4872333c

--- game/src/nemu-pal/hal/timer.c
+++ game/src/nemu-pal/hal/timer.c
@@ -25,7 +25,7 @@ timer_event(void) {
 
 uint32_t SDL_GetTicks() {
 	/* TODO: Return the time in millisecond. */
-	Log("%d", jiffy);
+//	Log("%d", jiffy);
 	return jiffy * 10;
 	assert(0);
 	return 0;
