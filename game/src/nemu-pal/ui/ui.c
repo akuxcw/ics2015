9b61686fb855b81c6874577551f808c2a29c453f
tracer-ics2015 <tracer@njuics.org>
2015-12-28 06:24:33 +0800

    >  compile game 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:24:33 up  1:58,  1 user,  load average: 0.12, 0.12, 0.17 a4565a4be0e096913e973493101a31f2fee432d0

--- game/src/nemu-pal/ui/ui.c
+++ game/src/nemu-pal/ui/ui.c
@@ -719,10 +719,10 @@ PAL_LoadObjectDesc(
    //
    while (fgets(buf, 512, fp) != NULL)
    {
-Log("^^^^^^^^^^^^^^");
       p = strchr(buf, '=');
       if (p == NULL)
       {
+Log("^^^^^^^^^^^^^^");
          continue;
       }
 
