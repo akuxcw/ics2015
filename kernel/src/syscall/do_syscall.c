d7c1502b67bbd09996059e9b124e5d030ffdd00b
tracer-ics2015 <tracer@njuics.org>
2015-12-28 02:57:44 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  02:57:44 up  3:33,  1 user,  load average: 0.06, 0.07, 0.06 f12de380273296d240df9f1246cc088180d0b831

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -6,6 +6,12 @@ void add_irq_handle(int, void (*)(void));
 void mm_brk(uint32_t);
 void serial_printc(char);
 
+int fs_open(const char *pathname, int flags);
+int fs_read(int fd, void *buf, int len);
+int fs_write(int fd, void *buf, int len);
+int fs_lseek(int fd, int offset, int whence);
+int fs_close(int fd);
+
 static void sys_brk(TrapFrame *tf) {
 #ifdef IA32_PAGE
 	mm_brk(tf->ebx);
@@ -39,6 +45,10 @@ void do_syscall(TrapFrame *tf) {
 
 		case SYS_brk: /*panic("@@@");*/sys_brk(tf); break;
 		case SYS_write: sys_write(tf); break;
+		case SYS_open : tf->eax = fs_open((char *)tf->ecx, tf->edx); break;
+		case SYS_read : tf->eax = fs_read(tf->ebx, (void *)tf->ecx, tf->edx); break;
+		case SYS_lseek : tf->eax = fs_lseek(tf->ebx, tf->ecx, tf->edx); break;
+		case SYS_close : tf->eax = fs_close(tf->ebx); break;
 
 		/* TODO: Add more system calls. */
 
