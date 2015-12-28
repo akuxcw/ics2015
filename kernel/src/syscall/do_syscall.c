75d388c57e183061b787c32cfd149872c5442ca8
tracer-ics2015 <tracer@njuics.org>
2015-12-29 01:24:52 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  01:24:52 up 4 min,  1 user,  load average: 0.21, 0.11, 0.05 8c0d2c55605952598404c40cd3ad30a4e88eabab

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -46,10 +46,10 @@ void do_syscall(TrapFrame *tf) {
 		case SYS_brk: /*panic("@@@");*/sys_brk(tf); break;
 		case SYS_write: sys_write(tf); break;
 		case SYS_open : 
-			tf->eax = fs_open((char *)tf->ebx, tf->edx); break;
+			tf->eax = fs_open((char *)tf->ebx, tf->ecx); break;
 		case SYS_read : 
-			Log("%x %x %x %x %x\n", tf->ebx, tf->ecx, tf->edx, tf->edi, tf->esi);
-			tf->eax = fs_read(tf->ebx, (void *)tf->esi, tf->edx); break;
+//			Log("%x %x %x %x %x\n", tf->ebx, tf->ecx, tf->edx, tf->edi, tf->esi);
+			tf->eax = fs_read(tf->ebx, (void *)tf->ecx, tf->edx); break;
 		case SYS_lseek : tf->eax = fs_lseek(tf->ebx, tf->ecx, tf->edx); break;
 		case SYS_close : tf->eax = fs_close(tf->ebx); break;
 
