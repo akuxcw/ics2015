de4f5f06a7cd53e01d6cf9b11a49f4ef2aa0a4a6
tracer-ics2015 <tracer@njuics.org>
2015-12-30 06:11:01 +0800

    >  compile kernel 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:11:01 up 46 min,  1 user,  load average: 0.17, 0.14, 0.17 b424e6737723db757512a481cf40bf8be30dda26

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -61,7 +61,7 @@ int fs_open(const char *pathname, int flags) {
 
 int fs_read(int fd, void *buf, int len){
 //	Log("%s", file_table[fd-3].name);
-	Log("%x %x %x", fd, (int)buf, len);
+//	Log("%x %x %x", fd, (int)buf, len);
 	if(!FD[fd].opened) return -1;
 	if(FD[fd].offset + len > file_table[fd-3].size) return -1;
 	//HIT_GOOD_TRAP;
