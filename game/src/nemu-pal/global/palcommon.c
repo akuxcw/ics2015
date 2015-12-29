8da6ad287534d1f722fdd2205e3ed703d9e019ae
tracer-ics2015 <tracer@njuics.org>
2015-12-30 06:42:22 +0800

    >  compile game 141220012 akuxcw Linux Akuxcw 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  06:42:22 up  1:17,  1 user,  load average: 0.00, 0.05, 0.11 dbaa13a09874f5d5320cca5632048e08ee2ae46e

--- game/src/nemu-pal/global/palcommon.c
+++ game/src/nemu-pal/global/palcommon.c
@@ -714,7 +714,7 @@ PAL_MKFGetChunkSize(
    uiChunkCount = PAL_MKFGetChunkCount(fp);
    if (uiChunkNum >= uiChunkCount)
    {
-	   HIT_GOOD_TRAP;
+//	   HIT_GOOD_TRAP;
       return -1;
    }
 
@@ -724,8 +724,10 @@ PAL_MKFGetChunkSize(
    fseek(fp, 4 * uiChunkNum, SEEK_SET);
    fread(&uiOffset, sizeof(UINT), 1, fp);
    fread(&uiNextOffset, sizeof(UINT), 1, fp);
+   Log("%x %x", uiNextOffset, uiOffset);
    uiOffset = SWAP32(uiOffset);
    uiNextOffset = SWAP32(uiNextOffset);
+   Log("%x %x", uiNextOffset, uiOffset);
 
    //
    // Return the length of the chunk.
